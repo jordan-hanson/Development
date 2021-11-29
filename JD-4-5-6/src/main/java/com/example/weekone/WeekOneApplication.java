4164df87e2f228fae307b84e5be1351b0d5377ff
Jordan Hanson <jordanmercedes22@gmail.com>
2021-11-29 09:42:13 -0700

    Implemented Unit Test Ready for Review

--- JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
+++ JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
@@ -6,24 +6,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeekOneApplication {
 
-    public static void Fibonacci(int firstTerm, int secondTerm, int lengthOfSequence){
+    public static void main(String[] args) {
+        SpringApplication.run(WeekOneApplication.class, args);
+        fibonacci(0, 1, 13);
+    }
+
+    public static String fibonacci(int firstTerm, int secondTerm, int lengthOfSequence){
         // Example: firstTerm = 0 secondTerm = 1 lengthOfSequence= 13
+        String result = "";
         for ( int i = 1; i <= lengthOfSequence; i++){
             if(i == lengthOfSequence){
+                result += firstTerm;
                 System.out.println(firstTerm);
             } else{
+                result += firstTerm + ", ";
                 System.out.print(firstTerm + ", ");
             }
             int nextTerm = firstTerm + secondTerm;
             firstTerm = secondTerm;
             secondTerm = nextTerm;
         }
-
+        return result;
     }
-
-    public static void main(String[] args) {
-        SpringApplication.run(WeekOneApplication.class, args);
-        Fibonacci(0, 1, 13);
-    }
-
 }
--- JD-4-5-6/src/test/java/com/example/weekone/WeekOneApplicationTests.java
+++ JD-4-5-6/src/test/java/com/example/weekone/WeekOneApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.weekone;
 
+import com.sun.tools.javac.Main;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeekOneApplicationTests {
 
     @Test
-    void contextLoads() {
+    void fibonacciNumbers() {
+        int startingNumber = 0;
+        int nextNumber = 1;
+        int totalInSequence = 13;
+        String expectedOutput = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
+
+        assertEquals(expectedOutput, WeekOneApplication.fibonacci(startingNumber, nextNumber, totalInSequence));
     }
+
 
 }
