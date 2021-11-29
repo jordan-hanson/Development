b5a1664b38b46fa81bd8c2eac8901888a0d18278
Jordan Hanson <jordanmercedes22@gmail.com>
2021-11-29 09:18:01 -0700

    Fibonnaci JD-4 Ready

--- JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
+++ JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
@@ -6,10 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeekOneApplication {
 
+    public static void Fibonacci(int firstTerm, int secondTerm, int lengthOfSequence){
+        // Example: firstTerm = 0 secondTerm = 1 lengthOfSequence= 13
+        for ( int i = 1; i <= lengthOfSequence; i++){
+            if(i == lengthOfSequence){
+                System.out.println(firstTerm);
+            } else{
+                System.out.print(firstTerm + ", ");
+            }
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
 
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WeekOneApplication.class, args);
+        Fibonacci(0, 1, 13);
     }
 
 }
