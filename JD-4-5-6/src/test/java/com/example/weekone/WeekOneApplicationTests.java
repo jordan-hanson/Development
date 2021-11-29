594fed1e4250c6729ce26f338cb227ec3e961fdd
Jordan Hanson <jordanmercedes22@gmail.com>
2021-11-29 10:05:34 -0700

    Updated Return as an ArrayList Ready for Review

--- JD-4-5-6/src/test/java/com/example/weekone/WeekOneApplicationTests.java
+++ JD-4-5-6/src/test/java/com/example/weekone/WeekOneApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootTest
 class WeekOneApplicationTests {
 
@@ -13,9 +16,8 @@ class WeekOneApplicationTests {
         int startingNumber = 0;
         int nextNumber = 1;
         int totalInSequence = 13;
-        String expectedOutput = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144";
-
-        assertEquals(expectedOutput, WeekOneApplication.fibonacci(startingNumber, nextNumber, totalInSequence));
+        ArrayList<Integer> expectedOut = new ArrayList<Integer>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144));
+        assertEquals(expectedOut, WeekOneApplication.fibonacci(startingNumber, nextNumber, totalInSequence));
     }
 
 
