6add80615ddc0c3e68c19b057621c2f82a24e2c6
Jordan Hanson <jordanmercedes22@gmail.com>
2021-11-29 08:56:24 -0700

    Start of Application

---
+++ JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
@@ -0,0 +1,15 @@
+package com.example.weekone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekOneApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekOneApplication.class, args);
+    }
+
+}
