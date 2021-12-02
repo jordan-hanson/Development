74ef830c3d143cc28468574c5843efeb3d7275a0
Jordan Hanson <jordanmercedes22@gmail.com>
2021-12-02 11:17:31 -0700

    Updated modifications to dependencies and generateFibonacciSequence

--- JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
+++ JD-4-5-6/src/main/java/com/example/weekone/WeekOneApplication.java
@@ -1,29 +1,26 @@
 package com.example.weekone;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
 public class WeekOneApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeekOneApplication.class, args);
-        fibonacci(0, 1, 13);
     }
 
-    public static ArrayList fibonacci(int firstNumber, int secondNumber, int lengthOfSequence){
-        // Example: firstNumber = 0 secondNumber = 1 lengthOfSequence= 13
-        ArrayList result = new ArrayList();
+    /**
+     *
+     * Definition: Used to generate a Fibonnaci Sequence starting at 0 and 1, based on the length you provide.
+     * @param lengthOfSequence
+     * @return ArrayList<Integer>
+     */
+    public static List<Integer> generateFibonacciSequence(int lengthOfSequence){
+        // Example: lengthOfSequence= 13
+        int firstNumber = 0;
+        int secondNumber = 1;
+        List<Integer> result = new ArrayList<>();
         for ( int i = 1; i <= lengthOfSequence; i++){
-            if(i == lengthOfSequence){
-                result.add(firstNumber);
-                System.out.println(firstNumber);
-            } else{
-                result.add(firstNumber);
-                System.out.print(firstNumber + ", ");
-            }
+            result.add(firstNumber);
             int nextNumber = firstNumber + secondNumber;
             firstNumber = secondNumber;
             secondNumber = nextNumber;
