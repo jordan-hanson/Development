a420a68945d2a03714b8295a5311ff6cf7ea04e8
Jordan Hanson <jordanmercedes22@gmail.com>
2021-12-06 10:13:08 -0700

    Updated Tests to include only 12 Integers for a test sequence

--- JD-4-5-6/src/test/java/com/example/weekone/WeekOneApplicationTests.java
+++ JD-4-5-6/src/test/java/com/example/weekone/WeekOneApplicationTests.java
@@ -13,16 +13,16 @@ class WeekOneApplicationTests {
 
     @Test
     void generateFibonacciSequence() {
-        int totalInSequence = 13;
-        ArrayList<Integer> expectedOut = new ArrayList<Integer>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233));
+        int totalInSequence = 12;
+        ArrayList<Integer> expectedOut = new ArrayList<Integer>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144));
         assertEquals(expectedOut, WeekOneApplication.generateFibonacciSequence(totalInSequence));
     }
 
     @Test
     void reverseFibonacciSequence(){
-        int lengthOfSequence = 13;
+        int lengthOfSequence = 12;
         List<Integer> fibonacciSequence = WeekOneApplication.generateFibonacciSequence(lengthOfSequence);
-        ArrayList<Integer> expectedReverseOutput = new ArrayList<>(Arrays.asList(233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 1, 0));
+        ArrayList<Integer> expectedReverseOutput = new ArrayList<>(Arrays.asList(144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 1, 0));
         List<Integer> actualReverseOutput = WeekOneApplication.reverseArray(fibonacciSequence);
         assertEquals(expectedReverseOutput, actualReverseOutput);
     }
